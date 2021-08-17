e5d554c791a9be3de274a65dea580440e3450dbe
Alex021204 <alex77720040212@gmail.com>
2021-08-17 21:20:38 +0600

    тип возвращаемого значения метода
    тип возвращаемого значения метода

--- lessons_java/src/ClassesAndObjects.java
+++ lessons_java/src/ClassesAndObjects.java
@@ -3,13 +3,13 @@ public class ClassesAndObjects {
         Person person1 = new Person();
         person1.name = "Артем";
         person1.age = 29;
-        person1.sayHello();
-        person1.speak();
         Person person2 = new Person();
         person2.name = "Алекс";
         person2.age = 16;
-        person2.sayHello();
-        person2.speak();
+        int years1 = person1.calculateYearsToRetirement();
+        int years2 = person2.calculateYearsToRetirement();
+        System.out.println("Первому человеку до пенсии" +years1+"лет");
+        System.out.println("Второму человеку до пенсии" +years2+"лет");
     }
 }
 class Person{
@@ -18,6 +18,11 @@ class Person{
     //2. Действия, которые он совершает(методы)
     java.lang.String name;
     int age;
+
+    int calculateYearsToRetirement(){
+        int years = 65-age;
+        return years;
+    }
     void speak(){
         for(int i = 0; i < 3; i++){
         System.out.println("Меня зовут "+name +","+"мне "+age+"лет");}
