768d43c954a0456ee10a0a21215956b8b3839329
Alex021204 <alex77720040212@gmail.com>
2021-08-19 16:16:27 +0600

    сеттеры и геттеры
    сеттеры и геттеры

--- lessons_java/src/ClassesAndObjects.java
+++ lessons_java/src/ClassesAndObjects.java
@@ -1,35 +1,53 @@
 public class ClassesAndObjects {
     public static void main(String[] args){
         Person person1 = new Person();
-        person1.setNameAndAge("Артем", 30);
-        java.lang.String s1 = "Алекс";
-        Person person2 = new Person();
-        person2.setNameAndAge(s1, 16);
-        person1.speak();
-        person2.speak();
+        person1.setName("Какое-то имя");
+        person1.setAge(22);
+        System.out.println("Выводим значение в main методе:" + person1.getName());
+        System.out.println("Выводим значение в main методе:" + person1.getAge());
     }
 }
-class Person{
+class Person {
+    private java.lang.String name;
+    private int age;
+
     //У класса могут быть:
     //1. Данные(поля)
     //2. Действия, которые он совершает(методы)
-    java.lang.String name;
-    int age;
-
-    void setNameAndAge(java.lang.String username, int userage) {
-        name = username;
-        age = userage;
+    public void setName(java.lang.String userName) {
+        if (userName.isEmpty()) {
+            System.out.println("Ты не ввел имя");
+        } else {
+            name = userName;}
+    }
+    public java.lang.String getName() {       //внутреннюю перемену можно менять как угодно
+        return name;
     }
 
-    int calculateYearsToRetirement(){
-        int years = 65-age;
+    public void setAge(int userAge) {
+        if (userAge < 0){
+            System.out.println("Возраст должен быть положительным");
+        } else {
+        age = userAge;}
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    int calculateYearsToRetirement() {
+        int years = 65 - age;
         return years;
     }
-    void speak(){
-        for(int i = 0; i < 3; i++){
-        System.out.println("Меня зовут "+name +","+"мне "+age+"лет");}
+
+    void speak() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Меня зовут " + name + "," + "мне " + age + "лет");
+        }
     }
-    void sayHello(){
+
+    void sayHello() {
         System.out.println("Привет!");
     }
 }
