4fc2a0af6a975e2706a8a66fabfb87187c4bf359
Alex021204 <alex77720040212@gmail.com>
2021-08-21 17:59:02 +0600

    this
    this

---
+++ lessons_java/src/Lesson_this.java
@@ -0,0 +1,33 @@
+public class Lesson_this {
+    public static void main(String[] args){
+        Human human1 = new Human();
+        human1.setName("Tom");
+        human1.setAge(18);
+        human1.getInfo();
+        Human human2 = new Human();
+        human1.setName("Tom");
+        human1.setAge(18);
+        human1.getInfo();
+    }
+}
+class Human{
+    java.lang.String name;
+    int age;
+
+    public void setName(java.lang.String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public java.lang.String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void getInfo(){
+        System.out.println(name+","+age);
+    }
+ }
