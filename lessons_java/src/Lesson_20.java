80c8f664601d801e58b5bf5e0f28891c960197a8
Alex021204 <alex77720040212@gmail.com>
2021-08-24 16:40:03 +0600

    static
    static

--- lessons_java/src/Lesson_20.java
+++ lessons_java/src/Lesson_20.java
@@ -1,28 +1,36 @@
 import java.lang.String;
 public class Lesson_20 {
     public static void main(String[] args){
-        Appearance human1 = new Appearance();
+        Appearance a1 = new Appearance("Bob",40);
+        Appearance a2 = new Appearance("Tim",22);
+        a1.countNumberOfPeople();
+        a2.countNumberOfPeople();
+        Appearance a3 = new Appearance("Sergio",58);
+        a1.countNumberOfPeople();
+        a2.countNumberOfPeople();
+        a3.countNumberOfPeople();
     }
 }
 class Appearance {
     private String name;
     private int age;
 
-    public Appearance(){           //простой конструкторб который мы не видим
-        this.name = "имя поумолчанию";
-        this.age = 0;
+    private static int countPeople;
+    public Appearance(String name, int age) {
+        this.name = name;
+        this.age = age;
+        countPeople++;
     }
-    public Appearance(String name) {
-        System.out.println("привет из второго конструктора!");
+
+    public void setName(String name) {
         this.name = name;
     }
-    public  Appearance(String name, int age) {
-        System.out.println("привет из третьего конструктора!");
-        this.name = name;
+
+    public void getAge(int age) {
         this.age = age;
     }
 
-    public void setName(String name){this.name = name;}
-
-    public void getAge(int age){this.age = age;}
+    public void countNumberOfPeople(){
+        System.out.println("Number of people is " + countPeople);
+    }
 }
