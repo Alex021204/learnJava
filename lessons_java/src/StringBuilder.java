f0d4270c9146b675a9520ea1083d4c25e28346bb
Alex021204 <alex77720040212@gmail.com>
2021-08-25 20:39:26 +0600

    StringBuilder
    StringBuilder

---
+++ lessons_java/src/StringBuilder.java
@@ -0,0 +1,8 @@
+public class StringBuilder {
+    public static void main(String[] args){
+        java.lang.StringBuilder sb = new java.lang.StringBuilder("Hello");
+        System.out.println(sb.toString());
+        sb.append(" my").append(" friend");
+        System.out.println(sb.toString());
+    }
+}
