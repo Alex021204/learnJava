426f57163fa679e90ff9cd3b4459a898403eb570
Alex021204 <alex77720040212@gmail.com>
2021-08-26 20:16:11 +0600

    Форматирование строк в Java
    Форматирование строк в Java

---
+++ lessons_java/src/Lesson_23.java
@@ -0,0 +1,17 @@
+public class Lesson_23 {
+    public static void main(String[] args){
+        /*
+        System.out.printf("This is a string, %s","Nice");
+        System.out.printf("This is a string, %d", 10);
+        System.out.printf("%fThis %s is a string, %d", 10.5,"Hihi",10);
+        System.out.printf("String %10d \n", 10);
+        System.out.printf("String %10d \n", 100000);
+        System.out.printf("String %10d \n", 1000000000)
+         */
+        System.out.printf("This %.2f,", 44.5665);
+        System.out.printf("This %.2f,", 44.498799);
+        System.out.printf("This %.2f,", 44.2223364665);
+        System.out.printf("This %.2f,", 44.123);
+        // % - [10] , [.5]
+    }
+}
