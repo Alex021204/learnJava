4eb827c6acdb4ca5c1f79d684fdadeaa840a25d8
Alex021204 <alex77720040212@gmail.com>
2021-08-28 22:16:31 +0600

    Object
    Object

---
+++ lessons_java/src/Object_class.java
@@ -0,0 +1,7 @@
+import java.lang.String;
+public class Object_class {
+    public static void main(String[] args ){
+        String string = "Hello World!";
+        System.out.println(string);
+    }
+}
