ea69038385aeacffcb10d1cfc60b8265d7afec2e
Alex021204 <alex77720040212@gmail.com>
2021-08-31 17:13:51 +0600

    пакеты
    пакеты

---
+++ lessons_java/src/test.java
@@ -0,0 +1,12 @@
+import Forest.Tree;
+import Forest.Squirell;
+import java.util.*;
+
+public class test {
+    public static void main(String[] args){
+        Tree tree1 = new Tree();
+        Squirell squirell1 = new Squirell();
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}
