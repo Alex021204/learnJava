e5a1741ffc42a3290b685bf916437acf8995aa22
Alex021204 <alex77720040212@gmail.com>
2021-09-03 11:44:58 +0600

    Полиморфизм
    Полиморфизм

---
+++ lessons_java/src/Interfaces_1/Cat.java
@@ -0,0 +1,7 @@
+package Interfaces_1;
+
+public class Cat extends Animal {
+    public void eat() {
+        System.out.println("Cat is eating");
+    }
+}
---
+++ lessons_java/src/Interfaces_1/Test.java
@@ -0,0 +1,21 @@
+package Interfaces_1;
+
+public class Test {
+    public static void main(String[] args) {
+        /*
+        Animal animal = new Dog();
+        animal.eat();
+         */
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        test(animal);
+        test(dog);
+        test(cat);
+    }
+    public static void test(Animal animal) {
+        animal.eat();
+
+    }
+}
