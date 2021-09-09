73bdc360c323f1313eb366d5e0db275eeebc0afc
Alex021204 <alex77720040212@gmail.com>
2021-09-09 14:23:36 +0600

    Классы-обертки примитивных типов данных
    Классы-обертки примитивных типов данных

---
+++ lessons_java/src/Interfaces_3/Test.java
@@ -0,0 +1,11 @@
+package Interfaces_3;
+
+public class Test {
+    public static void main(String[] args){
+        int x = 1;
+
+        Integer x2 = 123;
+        int x3 = x2; // распаковка Integer
+        // Double, Float, Long, Integer, Short, Byte, Character, Boolean
+    }
+}
