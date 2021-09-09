4212234cd90e498d64e1cd95d0f02e85f87c4196
Alex021204 <alex77720040212@gmail.com>
2021-09-09 14:08:43 +0600

    Приведение примитивных типов данных
    Приведение примитивных типов данных

---
+++ lessons_java/src/Interfaces_2/Test.java
@@ -0,0 +1,38 @@
+package Interfaces_2;
+
+public class Test {
+    public static void main(String[] args) {
+        /* примитивные типы данных
+        byte x = 123; // -128 до 127
+        long y = 123;
+        double a = 12.3;
+        float s = 12.3f;
+
+         */
+        /*
+        float f = (float)123.3;
+
+        int a = 123;
+
+        long l = a; //неявное
+        int x = (int)l; //явное
+        System.out.println(x);
+
+         */
+        /*
+        double x = 123.5;
+        int a = (int)x;
+
+        System.out.println(x);
+
+        long l  = Math.round(x);
+        System.out.println(l);
+
+         */
+        byte x = (byte)128;
+        System.out.println(x);
+
+
+
+    }
+}
